3837a39cfa7fd662ae52aa89504a38b729f7ce6b
spirit-drive <spirit-drive@yandex.ru>
2017-11-16 17:04:48 +0300

    Обновление

--- app/blocks/management/management.js
+++ app/blocks/management/management.js
@@ -1,4 +1,5 @@
-(function () {'use strict';
+'use strict';
+(function () {
     const SLIDER = document.getElementById('management__slider');
     const RUNNER = document.getElementById('management__runner');
     const PRECISE_CONTROL = document.getElementById('management__precise-control');
@@ -7,63 +8,108 @@
     const HALF_RUNNER = RUNNER.offsetWidth / 2;
     const SLIDER_MIN = 1;
     const SLIDER_MAX = 100;
-    const SLIDER_DEFAULT = 50;
+    const SLIDER_DEFAULT = 10;
     let sliderWidth = SLIDER.offsetWidth;
     SLIDER_START_TEXT.textContent = SLIDER_MIN;
     SLIDER_FINISH_TEXT.textContent = SLIDER_MAX;
-    // Положение бегунка
-    let posRunner = e =>{
-        let pos = e.pageX - SLIDER.offsetLeft - HALF_RUNNER;
-        return (pos < -HALF_RUNNER)
-                    ? -HALF_RUNNER
-                    :(pos > sliderWidth - HALF_RUNNER)
-                        ? sliderWidth - HALF_RUNNER
-                        : pos;
+
+// Куда поставить бегунок. Возвращает численное значение left
+    let posRunner = e => {
+        return e.pageX - SLIDER.offsetLeft - HALF_RUNNER;
     };
-    // Значение слайдера по положению бегунка orientation = true и положение бегулка по значению слайдера orientation = false
+
+// Значение слайдера по положению бегунка orientation = true и положение бегулка по значению слайдера orientation = false
     let valueSlider = (val,orientation = true) =>{
         return orientation
                     ? Math.round((val + HALF_RUNNER)*(SLIDER_MAX - SLIDER_MIN) / (sliderWidth ) + 1)
                     : ((val-1)*sliderWidth/(SLIDER_MAX - SLIDER_MIN)) - HALF_RUNNER;
     }
-// Параметры слайдера, которые исполняются много раз
-    let sliderParams = (pos,val) =>{
-        RUNNER.style.left = pos + 'px';
+// Ограничивает значение в заданом диапазоне и возвращает значение
+    let limiter = (val,min,max) =>{
+        return (val > max)
+                    ? max
+                    : (val < min)
+                        ? min
+                        : val;
+    }
+
+// left принимает числовое значение и устанавливает left бегунка, так же по нему находится числовое значение input.
+// val числовое значение слайдера, если оно не объявлено, функция сама вычисляет его и устанавливает в input
+    let sliderParams = (left,val) =>{
+        left = limiter(left,-HALF_RUNNER,sliderWidth - HALF_RUNNER);
+        RUNNER.style.left = `${left}px`;
         (!!val)
             ? PRECISE_CONTROL.value = val
-            : PRECISE_CONTROL.value = valueSlider(pos);
+            : PRECISE_CONTROL.value = valueSlider(left);
     }
-// Помощник слайдера
+
+// Принимает числовое значение слайдера, выставляет бегунок в соответсвии со значением
     let helperSliderParams = (val,variant = false)=>{
         sliderParams(valueSlider(val,variant),val);
-    }
-// Выставляем по дефолту
+    };
+
+// Выставляет бегунок по дефолту
     helperSliderParams(SLIDER_DEFAULT);
+
 // Управление слайдера мышкой
     SLIDER.onmousedown = function (e) {
         sliderParams(posRunner(e));
         document.onmousemove = function (c) {
             sliderParams(posRunner(c));
-        }
+        };
         document.onmouseup = function () {
             document.onmousedown = document.onmousemove = null;
         };
     };
+
 // Управление слайдера текстовым полем
     PRECISE_CONTROL.onkeyup = function () {
         setTimeout(function () {
-            (PRECISE_CONTROL.value > SLIDER_MAX)
-                ? helperSliderParams(SLIDER_MAX)
-                : (PRECISE_CONTROL.value < SLIDER_MIN)
-                    ? helperSliderParams(SLIDER_MIN)
-                    : RUNNER.style.left = valueSlider(PRECISE_CONTROL.value, false) + 'px';
+            let helper = limiter(PRECISE_CONTROL.value,SLIDER_MIN,SLIDER_MAX);
+            if (helper === SLIDER_MAX){
+                helperSliderParams(SLIDER_MAX);
+            }else if(helper === SLIDER_MIN){
+                helperSliderParams(SLIDER_MIN);
+            }
+            RUNNER.style.left = `${valueSlider(helper, false)}px`;
         },1000);
     };
-    // При изменении окна
+
+// При изменении окна
     window.onresize = function () {
         // Минимум, максимум и положение бегунка вычисляется
         sliderWidth = SLIDER.offsetWidth;
         helperSliderParams(PRECISE_CONTROL.value);
+    };
+
+// При фокусе на бегунке, можем двигать его стрелками
+    RUNNER.onfocus = function () {
+        const STEP = 2;
+        let speedup = 0;
+    // Для вычисления left
+        let posRun = () =>{
+            return this.style.left.match(/^-?\d+/)[0];
+        };
+    // Добавляет ускорение, принимает направление движения
+        let moveKeyboard = speed =>{
+            speedup += speed;
+            this.style.left = `${sliderParams(Number(posRun()) + speedup*STEP)}px`;
+        };
+        this.onkeydown = function (e) {
+            switch (e.keyCode) {
+                case 37:
+                    moveKeyboard(-1);
+                    break;
+                case 39:
+                    moveKeyboard(1);
+                    break;
+            }
+        };
+    // Обнуляет ускорение
+        this.onkeyup = function () {
+            speedup = 0;
+        };
+
     };
 
 }());
